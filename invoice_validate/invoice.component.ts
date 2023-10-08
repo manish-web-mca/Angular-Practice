84779620dd7b17aae12e8d23908dbdd7bcd80318
manish-web-mca <61628299+manish-web-mca@users.noreply.github.com>
2023-10-08 19:39:05 +0530

    Add files via upload

---
+++ invoice_validate/invoice.component.ts
@@ -0,0 +1,133 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms' 
+
+@Component({
+  selector: 'app-invoice',
+  templateUrl: './invoice.component.html',
+  styleUrls: ['./invoice.component.css']
+})
+export class InvoiceComponent implements OnInit {
+  productForm: FormGroup;
+  @Output() saveInvoice = new EventEmitter<any>();
+  sampleFormData = {
+    "subtotal": "180",
+    "totalTax": "40",
+    "grandTotal": "180",
+    "invoices": [
+        {
+            "productName": "Item 1",
+            "quantity": "5",
+            "rate": "9",
+            "saleTax": "10",
+            "total": 45
+        },
+        {
+          "productName": "Item 2",
+          "quantity": "5",
+          "rate": "9",
+          "saleTax": "10",
+          "total": 45
+      },
+      {
+        "productName": "Item 3",
+        "quantity": "5",
+        "rate": "9",
+        "saleTax": "10",
+        "total": 45
+    },
+    {
+      "productName": "Item 4",
+      "quantity": "5",
+      "rate": "9",
+      "saleTax": "10",
+      "total": 45
+    }
+    ]
+};
+
+  constructor(private fb:FormBuilder) { 
+    this.productForm = this.fb.group({ 
+      subtotal: '',
+      totalTax: '',
+      grandTotal: '',
+      invoices: this.fb.array([]) ,  
+    })
+
+  }
+
+  invoices() : FormArray {  
+    return this.productForm.get("invoices") as FormArray  
+  }  
+
+  newInvoice(): FormGroup {  
+    return this.fb.group({  
+      productName: ['', Validators.required], 
+      quantity: ['', Validators.required],
+      rate: ['', Validators.required],
+      saleTax: ['', Validators.required],
+      total: ['', Validators.required],  
+    })  
+  }  
+
+  rebuildForm() {
+    this.productForm.setValue(this.sampleFormData);    
+  }
+
+  ngOnInit(): void {
+   /* if(this.sampleFormData.invoices.length > 0) {
+      this.sampleFormData.invoices.forEach(() => {
+        this.addInvoice();
+      })
+    } else {
+      this.addInvoice();
+    }  */
+    this.addInvoice();
+   //this.rebuildForm();   
+  }
+
+  
+
+  onSubmit() {  
+    this.saveInvoice.emit(this.productForm.value);
+    //console.log(this.productForm.value);  
+  } 
+
+  addInvoice() {  
+    this.invoices().push(this.newInvoice());  
+  }  
+     
+  removeInvoice(i:number) {  
+    this.invoices().removeAt(i);  
+    this.calGrandTotal();
+  } 
+
+  calTotal(item: any, index: number) {
+    let rate: number = item.value.rate == '' ? 0 :item.value.rate; 
+    let quantity: number = item.value.quantity == '' ? 0 :item.value.quantity; 
+    let saleTax: number = item.value.saleTax == '' ? 0 :item.value.saleTax; 
+    let total: number = item.value.total == '' ? 0 :item.value.total; 
+    let saletaxAmount: number = 0;
+    if(saleTax > 0) {
+      saletaxAmount = ((rate * quantity) * saleTax) / 100;
+    }
+   
+    
+    item.patchValue({'total' : (rate * quantity) + saletaxAmount});
+    this.calGrandTotal();
+   
+  }
+
+  calGrandTotal() {
+    
+    let totalItem = this.productForm.value.invoices;
+    let tot = 0;
+    let saleTax = 0;
+    
+    totalItem.forEach((item: any) => {
+      tot += item.total;
+      saleTax += item.saleTax;
+    });
+    this.productForm.patchValue({'subtotal': tot, 'totalTax': saleTax, 'grandTotal': tot});
+  }
+
+}
